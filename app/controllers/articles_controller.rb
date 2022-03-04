b68af59e4483c308dd5f9412a1cabd2e5e6525cb
shimaahmedd <shimaahmedd@gmail.com>
2022-03-04 20:47:51 +0200

    Added articles list feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,13 @@ class ArticlesController < ApplicationController
 #   before_action :set_article, only: %i[ show edit update destroy ]
 
 #   # GET /articles or /articles.json
-#   def index
-#     @articles = Article.all
-#   end
+  def index
+    @articles = Article.all
+  end
 
 #   # GET /articles/1 or /articles/1.json
   def show
-    @article = Article.find(params[:id])
+    @articles = Article.all
   end
 
 #   # GET /articles/new
