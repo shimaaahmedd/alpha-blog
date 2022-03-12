c8ea9f420db27ac4a20d5050740bdf610a3c1d8a
shimaahmedd <shimaahmedd@gmail.com>
2022-03-12 11:49:03 +0200

    changes to main

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_085845) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_12_093044) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
