ba7dc6286870fb947243e57b55f9b2e450f5bffd
shimaahmedd <shimaahmedd@gmail.com>
2022-03-12 14:15:37 +0200

    Add edit template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_03_12_093044) do
+=======
+ActiveRecord::Schema[7.0].define(version: 2022_03_12_103918) do
+>>>>>>> users-signup
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_12_093044) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
