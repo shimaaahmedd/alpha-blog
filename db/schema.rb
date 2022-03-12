69ad1c1b6811a0a882a7c52854d23017b92b0d21
shimaahmedd <shimaahmedd@gmail.com>
2022-03-12 18:11:41 +0200

    admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_093044) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_03_12_103918) do
->>>>>>> users-signup
+ActiveRecord::Schema[7.0].define(version: 2022_03_12_155209) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -29,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_12_103918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
